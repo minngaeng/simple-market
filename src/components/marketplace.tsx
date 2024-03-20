30b3b7cd2f72b704a187eb334ec1ee745c8645b8
minngaeng <minngaeng@gmail.com>
2024-03-21 00:16:14 +0900

    ADD:카테고리UI

--- src/components/marketplace.tsx
+++ src/components/marketplace.tsx
@@ -1,18 +1,42 @@
+import styled from 'styled-components';
+
+const CategoryWrapper = styled.div`
+    list-style: none;
+`;
+
+const CategoryContents = styled.p`
+    margin: 10px 0;
+    height: 30px;
+    border-radius: 15px;
+    border: 1px solid lightgray;
+    cursor: pointer;
+    &:hover {
+        background-color: #ededed;
+    }
+`;
+
+const MoreButton = styled.button`
+    width: 100%;
+`;
+
 import { useGetCategories } from '../hooks/useGetCategories.ts';
 
 const Marketplace = () => {
     const { categories } = useGetCategories();
-
+    console.log(categories);
     return (
         <div>
             <h1>Marketplace</h1>
             <div>
                 <h2>Filter</h2>
-                <ul>
+                <CategoryWrapper>
                     {categories.map((category) => (
-                        <li key={category.id}>{category.name}</li>
+                        <CategoryContents key={category.id}>
+                            {category.name}
+                        </CategoryContents>
                     ))}
-                </ul>
+                </CategoryWrapper>
+                <MoreButton>더 보기</MoreButton>
             </div>
         </div>
     );
