fc314bee377a264f5b23b583358014a59a610cca
minngaeng <minngaeng@gmail.com>
2024-03-21 20:19:07 +0900

    ADD:filter 기능

--- src/components/marketplace.tsx
+++ src/components/marketplace.tsx
@@ -46,31 +46,110 @@ const ApplyButton = styled.button`
 `;
 
 import { useGetCategories } from '../hooks/useGetCategories.ts';
+import { useEffect, useState } from 'react';
 
 const Marketplace = () => {
     const { categories } = useGetCategories();
-    console.log(categories);
+
+    const [visibleCategories, setVisibleCategories] = useState(
+        categories.slice(0, 5)
+    );
+    const [isAllCategoriesVisible, setIsAllCategoriesVisible] = useState(false);
+
+    const handleShowMoreClick = () => {
+        setVisibleCategories(categories);
+        setIsAllCategoriesVisible(true);
+    };
+
+    useEffect(() => {
+        const param = new URLSearchParams(window.location.search);
+        if (!param.has('categoryId')) {
+            window.history.pushState(null, '', window.location.pathname);
+        }
+    }, []);
+
+    const handleCategoryClick = (categoryId: number) => {
+        const param = new URLSearchParams(window.location.search);
+        param.set('categoryId', categoryId.toString());
+        const newRelativePathQuery =
+            window.location.pathname + '?' + param.toString();
+
+        window.history.pushState(null, '', newRelativePathQuery);
+    };
+
+    const [priceMin, setPriceMin] = useState<string>('');
+    const [priceMax, setPriceMax] = useState<string>('');
+
+    const handlePriceRangeClick = (min: number, max: number) => {
+        const params = new URLSearchParams(window.location.search);
+        params.set('price_min', min.toString());
+        params.set('price_max', max.toString());
+        updateURLParams(params);
+        setPriceMin('');
+        setPriceMax('');
+    };
+
+    const handlePriceInputChange = () => {
+        const params = new URLSearchParams(window.location.search);
+
+        if (priceMin && priceMax) {
+            if (priceMin) params.set('price_min', priceMin);
+            if (priceMax) params.set('price_max', priceMax);
+            updateURLParams(params);
+        }
+
+        setPriceMin('');
+        setPriceMax('');
+    };
+
+    const updateURLParams = (params: URLSearchParams) => {
+        const newRelativePathQuery =
+            window.location.pathname + '?' + params.toString();
+        window.history.pushState(null, '', newRelativePathQuery);
+    };
+
     return (
         <div>
             <h1>Marketplace</h1>
             <div>
                 <h2>Filter</h2>
                 <CategoryWrapper>
-                    {categories.map((category) => (
-                        <CategoryContents key={category.id}>
+                    {visibleCategories.map((category) => (
+                        <CategoryContents
+                            key={category.id}
+                            onClick={() => handleCategoryClick(category.id)}
+                        >
                             {category.name}
                         </CategoryContents>
                     ))}
-                    <MoreButton>더 보기</MoreButton>
+                    {!isAllCategoriesVisible && categories.length > 5 && (
+                        <MoreButton onClick={handleShowMoreClick}>
+                            더 보기
+                        </MoreButton>
+                    )}
                 </CategoryWrapper>
                 <PriceRangeWrapper>
                     <h3>가격</h3>
-                    <PriceRange>가격 전체</PriceRange>
-                    <PriceRange>10달러 ~ 30달러</PriceRange>
-                    <PriceRange>30달러 ~ 50달러</PriceRange>
+                    <PriceRange onClick={() => handlePriceRangeClick(10, 30)}>
+                        10달러 ~ 30달러
+                    </PriceRange>
+                    <PriceRange onClick={() => handlePriceRangeClick(30, 50)}>
+                        30달러 ~ 50달러
+                    </PriceRange>
                     <p>
-                        <PriceInput /> ~ <PriceInput /> 원{' '}
-                        <ApplyButton>적용</ApplyButton>
+                        <PriceInput
+                            value={priceMin}
+                            onChange={(e) => setPriceMin(e.target.value)}
+                        />
+                        ~
+                        <PriceInput
+                            value={priceMax}
+                            onChange={(e) => setPriceMax(e.target.value)}
+                        />
+                        원
+                        <ApplyButton onClick={handlePriceInputChange}>
+                            적용
+                        </ApplyButton>
                     </p>
                 </PriceRangeWrapper>
             </div>
