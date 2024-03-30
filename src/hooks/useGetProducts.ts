99f4c14e7da6a25c9f968839f6b1a5e63b10892e
juepark <juepark@artue.io>
2024-03-30 11:40:19 +0900

    feat: use get products custom hook 작성

---
+++ src/hooks/useGetProducts.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { getProducts } from '../api/categories';
+import { Product } from '../types';
+
+export const useGetProducts = (query: string) => {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<boolean>(false);
+
+    useEffect(() => {
+        getProducts(query)
+            .then((data) => {
+                setProducts(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error getting products', error);
+                setError(true);
+                setLoading(false);
+            });
+    }, []);
+
+    return { products, loading, error };
+};
