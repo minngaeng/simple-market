990575b4e024b20810a2d77c6c9e1f8e13be7634
juepark <juepark@artue.io>
2024-03-30 11:41:14 +0900

    feat: get products 함수 작성

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -2,3 +2,8 @@ export const getCategories = async () => {
     const response = await fetch('https://api.escuelajs.co/api/v1/categories');
     return await response.json();
 };
+
+export const getProducts = async (query: string) => {
+    const response = await fetch(`https://api.escuelajs.co/api/v1/products?${query}`);
+    return await response.json();
+};
