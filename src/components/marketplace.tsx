ecfc2424192a3a1457d3ea1731e52d3bbcca5691
juepark <juepark@artue.io>
2024-03-30 11:41:46 +0900

    feat: marketplace 스타일 구조 변경

--- src/components/marketplace.tsx
+++ src/components/marketplace.tsx
@@ -1,15 +1,36 @@
 import PriceRangeWrapper from './priceRangeWrapper';
 import Category from './category';
+import ProductList from './productList';
 
 const Marketplace = () => {
 
     return (
-        <div>
+        <div
+            className='marketplace-container'
+            style={{
+                width: '100%',
+            }}>
             <h1>Marketplace</h1>
-            <div>
-                <h2>Filter</h2>
-                <Category />
-                <PriceRangeWrapper />
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between'
+            }}>
+                <div style={{
+                    border: '1px solid white',
+                    borderRadius: '5px',
+                    width: '200px',
+                }}>
+                    <Category />
+                    <PriceRangeWrapper />
+                </div>
+                <div style={{
+                    border: '1px solid white',
+                    borderRadius: '5px',
+                    width: 'calc(100% - 200px)',
+                }}>
+
+                    <ProductList />
+                </div>
             </div>
         </div>
     );
