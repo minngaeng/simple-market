8335eb364586d5cdb05d8572d0e05d7e9edbf333
juepark <juepark@artue.io>
2024-03-30 11:42:16 +0900

    product type 생성

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,12 @@ export type Category = {
     createdAt: string;
     updatedAt: string;
 };
+
+export type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: Pick<Category, 'id' | 'name' | 'image'>;
+    imgaes: string[];
+}
